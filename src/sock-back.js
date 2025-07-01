27c6a8b0c45ceb2a0e11a41d3e58673c408ef5e7
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-01 13:47:41 -0300

    feat: ensure create method CRUD

--- src/sock-back.js
+++ src/sock-back.js
@@ -1,41 +1,52 @@
+import {
+  adicionarDocumento,
+  atualizaDocumento,
+  encontraDocumento,
+  excluirDocumento,
+  obter_documentos,
+} from "./documentos-db.js";
 import { io } from "./servidor.js";
-
-const documentos = [
-  {
-    nome: "JavaScript",
-    texto: "texto de JavaScript",
-  },
-  {
-    nome: "Node",
-    texto: "texto de Node",
-  },
-  {
-    nome: "Socket.io",
-    texto: "texto de Socket.io",
-  },
-];
 
 io.on("connection", (socket) => {
   console.log("1 cliente se conectou!", socket.id);
+  socket.on("obter_documentos", async (devolverDocumentos) => {
+    const documentos = await obter_documentos();
+    devolverDocumentos(documentos);
+  });
 
-  socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+  socket.on("adicionar_documento", async (nome) => {
+    const documentoExiste = (await encontraDocumento(nome)) !== null;
+    if (documentoExiste) {
+      socket.emit("documento_existente", nome);
+    } else {
+      const resultado = await adicionarDocumento(nome);
+      if (resultado.acknowledged) {
+        io.emit("adicionar_documento_interface", nome);
+      }
+    }
+  });
+
+  socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
     socket.join(nomeDocumento);
-    const documento = encontraDocumento(nomeDocumento);
+    const documento = await encontraDocumento(nomeDocumento);
     if (documento) {
       devolverTexto(documento.texto);
     }
   });
 
-  socket.on("texto_editor", ({ texto, nomeDocumento }) => {
-    const documento = encontraDocumento(nomeDocumento);
-    if (documento) {
-      documento.texto = texto;
+  socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
+    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
+
+    if (atualizacao.modifiedCount) {
       socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
     }
   });
-});
 
-function encontraDocumento(nomeDocumento) {
-  const documento = documentos.find((a) => a.nome === nomeDocumento);
-  return documento;
-}
+  socket.on("excluir_documento", async (nome) => {
+    const result = await excluirDocumento(nome);
+    if(result.deletedCount){
+      io.emit("excluir_documento_sucesso")
+    }
+    
+  });
+});
